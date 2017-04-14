ab350c6e1525cc609fda317e2f99d3f6ace8050c
dimitri <ddiakopoulos@gmail.com>
2017-04-14 06:14:35 -0700

    added stb dependencies

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "linalg_util.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "third-party/stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "third-party/stb/stb_image_write.h"
+
 int main(int argc, char * argv[])
 {
     return EXIT_SUCCESS;
