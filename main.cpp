7c04af45e7a705395f12370145f3e2cc19b66f3b
dimitri <ddiakopoulos@gmail.com>
2017-04-14 06:32:37 -0700

    add skeleton of glfw window class

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <functional>
+#include <map>
 #include "linalg_util.hpp"
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -7,6 +10,54 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "third-party/stb/stb_image_write.h"
+
+#include "GLFW\glfw3.h"
+
+class Window
+{
+    GLFWwindow * window;
+public:
+    std::function<void(unsigned int codepoint)> on_char;
+    std::function<void(int key, int action, int mods)> on_key;
+    std::function<void(int button, int action, int mods)> on_mouse_button;
+    std::function<void(float2 pos)> on_cursor_pos;
+    std::function<void(int numFiles, const char ** paths)> on_drop;
+
+    Window(int width, int height, const char * title)
+    {
+        if (glfwInit() == GL_FALSE)
+        {
+            throw std::runtime_error("glfwInit() failed");
+        }
+
+        window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+
+        if (window == nullptr)
+        {
+            throw std::runtime_error("glfwCreateWindow() failed");
+        }
+    }
+
+    ~Window()
+    {
+
+    }
+
+    Window(const Window &) = delete;
+    Window(Window &&) = delete;
+    Window & operator = (const Window &) = delete;
+    Window & operator = (Window &&) = delete;
+
+    GLFWwindow * get_glfw_window_handle() { return window; };
+    bool should_close() const { return !!glfwWindowShouldClose(window); }
+    int get_window_attrib(int attrib) const { return glfwGetWindowAttrib(window, attrib); }
+    int2 get_window_size() const { int2 size; glfwGetWindowSize(window, &size.x, &size.y); return size; }
+    int2 get_framebuffer_size() const { int2 size; glfwGetFramebufferSize(window, &size.x, &size.y); return size; }
+    float2 get_cursor_pos() const { double2 pos; glfwGetCursorPos(window, &pos.x, &pos.y); return float2(pos); }
+
+    void swap_buffers() { glfwSwapBuffers(window); }
+    void close() { glfwSetWindowShouldClose(window, 1); }
+};
 
 int main(int argc, char * argv[])
 {
