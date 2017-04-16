375eabfa6946e1da2630d85dd0ed7993a176080c
dimitri <ddiakopoulos@gmail.com>
2017-04-15 19:09:27 -0700

    split out some functionality into util.hpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,7 @@
 #include <stdint.h>
 #include <complex>
 #include <type_traits>
-
-#include "linalg_util.hpp"
-
-#include "gli/gli.hpp"
+#include "util.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "third-party/stb/stb_image.h"
@@ -20,16 +17,14 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "third-party/stb/stb_image_write.h"
 
-#include "third-party/stb/stb_easy_font.h"
-
-#define GLEW_STATIC
-#define GL_GLEXT_PROTOTYPES
-#include "glew.h"
-
-#define GLFW_INCLUDE_GLU
-#include "GLFW\glfw3.h"
-
 #include "kissfft/kissfft.hpp"
+
+// enforce size of two
+// image pyramid for mips
+// cleanup all size.x, size.y types
+// generate mips
+// get rid of older load functions
+// dsa in texture_buffer
 
 inline void draw_text(int x, int y, const char * text)
 {
@@ -79,83 +74,6 @@ inline float as_float(const T & x)
     const float max = std::numeric_limits<T>::max();
     return (x - min) / (max - min);
 }
-
-class Window
-{
-    GLFWwindow * window;
-public:
-    std::function<void(unsigned int codepoint)> on_char;
-    std::function<void(int key, int action, int mods)> on_key;
-    std::function<void(int button, int action, int mods)> on_mouse_button;
-    std::function<void(float2 pos)> on_cursor_pos;
-    std::function<void(int numFiles, const char ** paths)> on_drop;
-
-    Window(int width, int height, const char * title)
-    {
-        if (glfwInit() == GL_FALSE)
-        {
-            throw std::runtime_error("glfwInit() failed");
-        }
-
-        window = glfwCreateWindow(width, height, title, nullptr, nullptr);
-
-        if (window == nullptr)
-        {
-            throw std::runtime_error("glfwCreateWindow() failed");
-        }
-
-        glfwMakeContextCurrent(window);
-
-        if (GLenum err = glewInit())
-        {
-            throw std::runtime_error(std::string("glewInit() failed - ") + (const char *)glewGetErrorString(err));
-        }
-
-        glfwSetCharCallback(window, [](GLFWwindow * window, unsigned int codepoint) { 
-            auto w = (Window *)glfwGetWindowUserPointer(window); if (w->on_char) w->on_char(codepoint); 
-        });
-
-        glfwSetKeyCallback(window, [](GLFWwindow * window, int key, int, int action, int mods) { 
-            auto w = (Window *)glfwGetWindowUserPointer(window); if (w->on_key) w->on_key(key, action, mods); 
-        });
-
-        glfwSetMouseButtonCallback(window, [](GLFWwindow * window, int button, int action, int mods) { 
-            auto w = (Window *)glfwGetWindowUserPointer(window); if (w->on_mouse_button) w->on_mouse_button(button, action, mods); 
-        });
-
-        glfwSetCursorPosCallback(window, [](GLFWwindow * window, double xpos, double ypos) { 
-            auto w = (Window *)glfwGetWindowUserPointer(window); if (w->on_cursor_pos) w->on_cursor_pos(float2(double2(xpos, ypos))); 
-        });
-
-        glfwSetDropCallback(window, [](GLFWwindow * window, int numFiles, const char ** paths) { 
-            auto w = (Window *)glfwGetWindowUserPointer(window); if (w->on_drop) w->on_drop(numFiles, paths); 
-        });
-
-        glfwSetWindowUserPointer(window, this);
-    }
-
-    ~Window()
-    {
-        glfwMakeContextCurrent(window);
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
-
-    Window(const Window &) = delete;
-    Window(Window &&) = delete;
-    Window & operator = (const Window &) = delete;
-    Window & operator = (Window &&) = delete;
-
-    GLFWwindow * get_glfw_window_handle() { return window; };
-    bool should_close() const { return !!glfwWindowShouldClose(window); }
-    int get_window_attrib(int attrib) const { return glfwGetWindowAttrib(window, attrib); }
-    int2 get_window_size() const { int2 size; glfwGetWindowSize(window, &size.x, &size.y); return size; }
-    int2 get_framebuffer_size() const { int2 size; glfwGetFramebufferSize(window, &size.x, &size.y); return size; }
-    float2 get_cursor_pos() const { double2 pos; glfwGetCursorPos(window, &pos.x, &pos.y); return float2(pos); }
-
-    void swap_buffers() { glfwSwapBuffers(window); }
-    void close() { glfwSetWindowShouldClose(window, 1); }
-};
 
 class texture_buffer
 {
@@ -356,7 +274,7 @@ int main(int argc, char * argv[])
 
     try
     {
-        win.reset(new Window(1280, 720, "image visualizer"));
+        win.reset(new Window(1280, 720, "image fft visualizer"));
     }
     catch (const std::exception & e)
     {
